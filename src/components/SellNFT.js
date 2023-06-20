542737507c20137f717540a2416cfe044f4fffb1
hrishikesh-d <72550381+hrishikesh-d@users.noreply.github.com>
2023-06-20 11:15:01 +0530

    Update SellNFT.js

--- src/components/SellNFT.js
+++ src/components/SellNFT.js
@@ -25,12 +25,12 @@ export default function SellNFT () {
         listButton.style.opacity = 1;
     }
 
-    //This function uploads the NFT image to IPFS
+    
     async function OnChangeFile(e) {
         var file = e.target.files[0];
-        //check for file extension
+        
         try {
-            //upload the file to IPFS
+            
             disableButton();
             updateMessage("Uploading image.. please dont click anything!");
             //calls fn- declared in pinata.js
@@ -47,10 +47,11 @@ export default function SellNFT () {
         }
     }
 
-    //This function uploads the metadata to IPFS
+    
     async function uploadMetadataToIPFS() {
         const {name, description, price} = formParams;
-        //Make sure that none of the fields are empty
+
+        
         if( !name || !description || !price || !fileURL)
         {
             updateMessage("Please fill all the fields!")
@@ -62,7 +63,8 @@ export default function SellNFT () {
         }
 
         try {
-            //upload the metadata JSON to IPFS
+
+            
             const response = await uploadJSONToIPFS(nftJSON);
             if(response.success === true){
                 console.log("Uploaded JSON to Pinata: ", response)
@@ -77,26 +79,30 @@ export default function SellNFT () {
     async function listNFT(e) {
         e.preventDefault();
 
-        //Upload data to IPFS
+
         try {
             const metadataURL = await uploadMetadataToIPFS();
             if(metadataURL === -1)
                 return;
-            //After adding your Hardhat network to your metamask, this code will get providers and signers
+
+            
             const provider = new ethers.providers.Web3Provider(window.ethereum);
             const signer = provider.getSigner();
             disableButton();
             updateMessage("Uploading NFT(takes 5 mins).. please dont click anything!")
 
-            //Pull the deployed contract instance
+
+            
             let contract = new ethers.Contract(Marketplace.address, Marketplace.abi, signer)
 
-            //massage the params to be sent to the create NFT request
+            
+            
             const price = ethers.utils.parseUnits(formParams.price, 'ether')
             let listingPrice = await contract.getListPrice()
             listingPrice = listingPrice.toString()
 
-            //actually create the NFT
+           
+            
             let transaction = await contract.createToken(metadataURL, price, { value: listingPrice })
             await transaction.wait()
 
